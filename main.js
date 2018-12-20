63231e3abbb3f5216552349cdf0e85b9e0304385
manduks <iam@armando.mx>
2018-12-19 19:45:27 -0600

    udpate of 2018

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const jsOutputEl = document.querySelector('.hljs');
 const skills = {
-  javascript: 90,
-  sencha: 80,
-  meteorJS: 80,
-  reactJS: 70,
-  reactNative: 70,
-  ui: 50,
-  ux: 50,
+  javascript: 95,
+  reactJS: 80,
+  reactNative: 80,
+  ui: 70,
+  ux: 70,
+  sencha: 70,
+  meteorJS: 70,
   ror: 40,
   php: 40
 };
@@ -25,12 +25,12 @@ const animageSkill = function animageSkill(level, selector) {
     }
   });
 };
-animageSkill(90, '.js');
-animageSkill(80, '.sencha');
-animageSkill(80, '.meteor');
-animageSkill(70, '.react');
-animageSkill(60, '.react-native');
-animageSkill(50, '.ui');
-animageSkill(50, '.ux');
+animageSkill(95, '.js');
+animageSkill(70, '.sencha');
+animageSkill(70, '.meteor');
+animageSkill(80, '.react');
+animageSkill(80, '.react-native');
+animageSkill(70, '.ui');
+animageSkill(70, '.ux');
 animageSkill(40, '.ror');
 animageSkill(40, '.php');
